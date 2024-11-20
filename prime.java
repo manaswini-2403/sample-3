40c189016f8d307e3f3672c42a1d0a47f70034f0
manaswini-2403 <sangepumanu24@gmail.com>
2024-11-20 22:29:54 +0530

    commit again

--- prime.java
+++ prime.java
@@ -1,18 +1,28 @@
-echo "public class Prime {
-  public static void main(String[] args) {
-      int num = 29;
-      boolean isPrime = true;
+import java.util.Scanner;
 
-      for (int i = 2; i <= num / 2; ++i) {
-          if (num % i == 0) {
-              isPrime = false;
-              break;
-          }
-      }
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
 
-      if (isPrime)
-          System.out.println(num + \" is a prime number.\");
-      else
-          System.out.println(num + \" is not a prime number.\");
-  }
-}" > prime.java
+        if (num <= 1) {
+            System.out.println(num + " is not a valid number for prime checking.");
+            return;
+        }
+
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(num); ++i) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
